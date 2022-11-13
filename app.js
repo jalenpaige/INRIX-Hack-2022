e05b9ffb3db4ad4c83937dc11244c83bf3b378e5
jalenpaige <117240799+jalenpaige@users.noreply.github.com>
2022-11-12 16:30:38 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,6 @@ const http = require('http');
 const fs = require('fs')
 const port = 3000;
 
-const url = 'http://localhost:8000/gettoken';
-fetch(url)
-.then(data => data.json())
-.then((json) => {
-    alert(JSON.stringify(json));
-})
 
 const server = http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'})
