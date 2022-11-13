d052793b27439cdff94c4250e05d63e9ee3f0bad
jalenpaige <117240799+jalenpaige@users.noreply.github.com>
2022-11-13 02:17:00 -0800

    Update app.js

--- app.js
+++ app.js
@@ -7,10 +7,8 @@ const port = 8000;
 app.set('json spaces', 2);
 app.use(bodyParser.json());
 const axios = require('axios');
-
 // to query, call: http://localhost:8000/gettoken
 const url = 'https://api.iq.inrix.com/findRoute';
-
 // const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHBJZCI6InhyNnhxbzViY2wiLCJ0b2tlbiI6eyJpdiI6IjYxM2M4MTVmY2ZhN2M3YWQzOGFkZWVmMjM5MmM4NDUwIiwiY29udGVudCI6ImM5MmIzYzEzYmE4ZmNiZWZhMWJjZWY3ZTZkNzUyNDExZDVmNjFhZWI1MTE5NTE1ZTE0MzhlZGUzYmM4ZTExNzE1YTYyOWVlZmNhNDA5ZmNmOWNhOWM0OGNiOTJmMTA0NWJkYWZlZjhlOTgwMzg0NDFjMTliNDFkODFmOGEyYzAzMGY0NzQ2YTY0NDc3YmU5ZDM3N2JiNTBkZWFiOWJjNzdkODhlM2Y5NGU5MjU4MTMzYmNjZTQ3MWFjYjdjOWE3MzMzZDcxNzY1YWFkZjI4YjQ2NGNkOTJiOTJmZjU4NjE1Y2M0ODY4MjJjNzFlODhkMjU1NjUwMzFjZTY3ZDg0MjgxMTRlN2I0YzlhMzZjNjczYWUxNTc3NDkxMWNmOTU4NjdiNTA3YzUwY2U1YTE1NDQzZmQyMmVlNDdjMmUyNWM1ODc1Nzg3NmQ3NjA2YzU5YzU0MTE5ZTBlYzZjOTIyYTYwZGE0NWFkNmZjMjA1ZjcxYjBhYWE0OTI3YjM2Y2JmNGMzN2U0ZjAyODVkYzNjYWYzZDMxZDRkY2E0NTQ1YTI1Mzg4ZTExMWFkZTU5MTc4M2ZmYmNhOTcwMmU2ZTE4ZTA4OWEwMDkzOTE5YmE0ZTczZmY3NzE2ZThjOTU4ZWQyNjdlODBiMzdlZDM1YzVkYWU5MGM4NTZmNTY4OGZjZDc2ZjM1NjgwYjg5Njk5OTk4MDZjM2I5YjIzY2U3ZGU0MmFiNDU1OWUzNWRlOGQ3Mjc3YWFhMTA0NGY2MTZlNWViZmVhOWVmOTgzNGY3M2RmODBmMzE1ZTMwYmMxZjQwMDIyYWI0MWIxNzFkNTliMzNiOGM1OTRhYzc2YTBlN2JmNzliNjIxMjkxMmQ2OGJmODdmOWFlZjgyIn0sInNlY3VyaXR5VG9rZW4iOnsiaXYiOiI2MTNjODE1ZmNmYTdjN2FkMzhhZGVlZjIzOTJjODQ1MCIsImNvbnRlbnQiOiJjYjMzMWUyOGVkYTRjN2Q2YWJiM2Y1NWM1MTQ2Mjk0ZmYyZDA2MWNjNGQxMzQwMzkxMzQ4OTNhM2Q4YmQyODdhNjM2OTk5OWE4MjE0OGNjMzg0YWJlYmIyIn0sImp0aSI6ImFmZGI5MjYyLWMzNGMtNDQxOS04ZjI4LWFiM2ZmNDZiZDhkMyIsImlhdCI6MTY2ODMwNjUzMywiZXhwIjoxNjY4MzEwMTMzfQ.ZYAqrwTHzoUwAWz5b4mgcpkqdrjmOjRgfTwHUfl89G4";
 
 const getToken = async () => {
@@ -28,7 +26,7 @@ const getToken = async () => {
     return output;
 }
 
-
+var min;
 app.get('/gettoken', async function (req, res) {
 
     //Set up URL to query
@@ -51,7 +49,8 @@ app.get('/gettoken', async function (req, res) {
         token: output,
     });
 })
-
+var times = new Array();
+var depTimes = new Array();
 app.get('/route', async function (req, res) {
 
     const params = req.query;
@@ -63,82 +62,98 @@ app.get('/route', async function (req, res) {
     d = new Date(start);
 
 
+
     //Increments by 60 minutes
     function addMinutes(date,minutes){
         return new Date(date.getTime()+minutes*60000);
     }
-    
+
     /*if (!params.wp_1 || !params.wp_2) {
         return res.json({
             "error": "missing params"
         })
     }*/
-    var times = new Array();
-    var eta_array = new Array();
-   for(let i = 0; i <= 9; i++)
+
+   for(let i = 0; i <= startTime; i++)
     {
         d = addMinutes(d,60);
     }
-    for(let j = 0; j <= 15-9; j++){
+
+    for(let j = 0; j <= endTime - startTime; j++){
         try {
             const token = await getToken();
             const response = await axios.get(url, {
                 params: {
-                    wp_1: "37.8044,-122.4370",
-                    wp_2: "37.7943, -122.4349",
+                    wp_1: lat1, long1,
+                    wp_2: lat2, long2,
                     departureTime: d,
                     format: "json",
                     routeOutputFields: "P"
                 },
                 headers: { Authorization: `Bearer ${token}` }
             });
-        
-            const data = response.data;
 
+            const data = response.data;
             const travelTimeMinutes = data.result.trip.routes[0].travelTimeMinutes;
             const uncongested = data.result.trip.routes[0].uncongestedTravelTimeMinutes;
-            const departureTime = params.departureTime;
-            
+
             times[j] = travelTimeMinutes;
+            depTimes[j] = d;
+
             d = addMinutes(d,60);
 
-            return res.json({
-                uncongestedTravelTime: uncongested,
-                travelTimeMinutes: travelTimeMinutes,
-                dpt: departureTime
-                
-            });
+            // return res.json({
+            //     uncongestedTravelTime: uncongested,
+            //     travelTimeMinutes: travelTimeMinutes,
+            //     //dpt: departureTime
+            //
+            // });
           } catch (error) {
                 console.error(error);
                 return res.json({
                     "error": "unknown error"
                 })
             }
-            
-            /*eta_array = travelTimeMinutes;
-            console.log("Estimated time of arrival is:"+eta_array[i]);*/
-            
+
+            //eta_array = travelTimeMinutes;
+            //console.log("Estimated time of arrival is:"+eta_array[i]);
+
     }
-    /*let f = times[0];
-    let ind = 0;
-    for(let k = 0; k < times.length; k++) {
+    let index = 0;
+    min = (Math.min(...times));
+    for (let i = 0; i < times.length; i++) {
+        if (times[i] == min) {
+            index = i;
+            break;
+          }
+    }
+    return deptTimes[index];
+
+    return("");
+    window.alert(times[0]);
+    let f = times[0];
+    let hourOfFastestTime = 0;
+    for(let k = 1; k < times.length; k++) {
         if(times[k] < f) {
             f = times[k];
-            ind = k;
+            hourOfFastestTime = k;
         }
-        console.log(times[k])
     }
-    ind = ind + 9;
+    hourOfFastestTime = hourOfFastestTime + 9;
 
     return res.json({
-        traveltimes: times
-    })*/
+        traveltimes: times[0]
+    })
+
     // f is the time it takes to get to the place
     // ind is the hour that the fastest time should be at
 })
+
 
 //Starting server using listen function
 app.listen(port, function () {
     console.log("Server has been started at " + port);
     console.log();
 })
+
+
